30569fdc996a9b69490eb484c03458712147e1e3
kimoony <hoonby9011@gmail.com>
2022-03-17 03:21:46 +0900

    create typescript practice

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${props => props.theme.backgroundColor};
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
