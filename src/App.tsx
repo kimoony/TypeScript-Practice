bbb917a679f5554003c356385cc3c619bc4fcfd1
kimoony <hoonby9011@gmail.com>
2022-03-19 01:01:02 +0900

    studied TypeScript props

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Circle from './Circle';
 function App() {
   return (
     <div>
-      <Circle bgColor="blue" />
-      <Circle bgColor="pink" />
+      <Circle borderColor="red" bgColor="blue" />
+      <Circle text="i'm here!" bgColor="pink" />
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,27 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// JavaScript에서는 propTypes로 prop을 확인해주었지만 
+// React를 할 땐 propTypes로 prop을 확인해주었지만 
 // 이제부턴 props를 TypeScript로 보호한다.
+
+// propTypes 와 TypeScript 차이
+// propTypes는 코드실행 후 브라우저에 에러가 나타난다.
+// TypeScript는 코드실행 전 에러를 확인할 수 있다.
+
 // 객체모양을 TypeScript에게 설명해주고 싶을 때 interface를 사용한다.
 
-interface CircleProps { // 이 객체 안에서 bgColor가 발견된다.
-  bgColor: string;  // bgColor는 string이 되어야해!
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
 }
 
-
-const Container = styled.div<CircleProps>`
+// component에도 bgColor를 받을거라고 TypeScript에게 알려주기 위해
+// CircleProps를 붙여준다.
+const Container = styled.div<ContainerProps>`
   width: 300px;
   height: 300px;
   background-color: ${props => props.bgColor};
   border-radius: 100%;
+  border: 2px solid ${props => props.borderColor}; // css 에서는 optional (x), requierd
 `;
 
+interface CircleProps { // 이 객체 안에서 bgColor가 발견된다.
+  bgColor: string;  // bgColor는 string이 되어야해! requierd
+  borderColor?: string;  // ? 를 넣으면 있어도 되고 없어도 된다는 의미이다. requierd 가 아니다.
+  // string | undefined
+  text?: string
+}
+
 // CircleProps 의 타입이 뭔지 component 에게 말해줘야 한다.
-function Circle({ bgColor }: CircleProps) { // bgColor의 타입은 CircleProps의 객체이다.
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) { // bgColor의 타입은 CircleProps의 객체이다.
   return (
-    <Container bgColor={bgColor} />
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
   )
 }
 
 export default Circle
+
+
